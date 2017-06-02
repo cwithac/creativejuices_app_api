2caacd63d9aa5ddb796606ec9340ba8cdd77ffd9
Cathleen Wright <cathleenmwright@gmail.com>
2017-06-02 09:06:39 -0400

    Cors privacy update

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,39 +7,39 @@
 
 require "rack/cors"
 
-Rails.application.config.middleware.insert_before ActionDispatch::Static, Rack::Cors do
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
+# Rails.application.config.middleware.insert_before ActionDispatch::Static, Rack::Cors do
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
-  # allow do
-  #   origins 'https://creative-juices.herokuapp.com/', 'localhost:3000'
-  #
-  #   resource '*',
-  #     headers: :any,
-  #     methods: [:get, :post, :put, :patch, :delete, :options, :head]
-  # end
+  allow do
+    origins 'https://creative-juices.herokuapp.com/'
 
-  # allow do
-  #   origins 'localhost:3000'
-  #
-  #   resource '*',
-  #     headers: :any,
-  #     methods: [:get, :post, :put, :patch, :delete, :options, :head]
-  # end
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
 
-  # allow do
-  #   origins '*'
-  #
-  #   resource '*',
-  #     headers: :any,
-  #     methods: [:get]
-  # end
+  allow do
+    origins 'localhost:3000'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
 
   allow do
     origins '*'
 
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :options, :head]
   end
+
+  # allow do
+  #   origins '*'
+  #
+  #   resource '*',
+  #     headers: :any,
+  #     methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  # end
 
 end
