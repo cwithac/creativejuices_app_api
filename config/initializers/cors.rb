660964292da4de64fe6f8288b159b1d718664f37
Cathleen Wright <cathleenmwright@gmail.com>
2017-06-02 09:08:33 -0400

    Cors privacy update

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -19,7 +19,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
-    origins 'localhost:3000'
+    origins 'localhost:8000'
 
     resource '*',
       headers: :any,
